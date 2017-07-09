50fcfe034a62da06bb95b071ca457d2f784fc871
Ridge Kimani <ridgekimani@gmail.com>
2017-07-09 21:13:19 +0300

    Some template changes.. Added Abstract features

--- app.py
+++ app.py
@@ -1,22 +1,209 @@
-from flask import Flask
-from flask import render_template
+from datetime import date
+
+from flask import request, render_template, Flask, jsonify, make_response, redirect, url_for, session
+
+from os import path
+
+import shelve
 
 
 app = Flask(__name__)
 
+SHELVE_DB = 'bucket_list_db'
 
-def hello_world():
-    return 'Hello World!'
+app.config.from_object(__name__)
+
+db = shelve.open(path.join(app.root_path, app.config['SHELVE_DB']), writeback=True)
+
+app.secret_key = 'this is a secret'
+
+
+def create_data(*args, **kwargs ):
+    data = AbstractFeatures(*args, **kwargs)
+    return data.create_data_()
+
+
+def delete_data(*args, **kwargs):
+    data = AbstractFeatures(*args, **kwargs)
+    return data.delete_data_()
+
+
+def update_data(*args, **kwargs):
+    data = AbstractFeatures(*args, **kwargs)
+    return data.update_data_()
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return redirect(url_for('login'))
 
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    return render_template('registration/register.html')
+    if request.method == 'GET':
+        return render_template('registration/register.html')
+
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        username = data.get('username')
+        password = data.get('password')
+        confirm_password = data.get('confirm_password')
+
+        if not username:
+            return make_response(jsonify({'error': 'Please enter your username'}), 500)
+
+        if not password:
+            return make_response(jsonify({'error': 'Please enter your password'}), 500)
+
+        if password != confirm_password:
+            return make_response(jsonify({'error': 'Passwords do not match'}), 500)
+
+        if username in db.keys():
+            return make_response(jsonify({'error': 'User already exists with that username'}), 500)
+
+        db[username] = password
+        session['user'] = username
+        return jsonify({'success': 'Account created successfully'})
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('registration/login.html')
+    if request.method == 'GET':
+        return render_template('registration/login.html')
+
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        username = data.get('username')
+        password = data.get('password')
+
+        if username not in db.keys():
+            return make_response(jsonify({'error': 'Username not found. Please sign up to continue'}), 500)
+
+        for key, value in db.items():
+            if key == username:
+                if value == password:
+                    session['user'] = username
+                    return jsonify({'success': 'Authenticated successfully'})
+                else:
+                    return make_response(jsonify({'error': 'Incorrect password'}), 500)
+
+            else:
+                return make_response(jsonify({'error': 'Username not found. Please sign up to continue'}), 500)
+
+
+@app.route('/logout', methods=['GET', 'POST'])
+def logout():
+    session.pop('user', None)
+    return redirect(url_for('login'))
+
+
+@app.route('/create_bucket', methods=['GET', 'POST'])
+def create_bucket():
+    if request.method == 'GET':
+        if 'user' in session.keys():
+            return render_template('create_bucket.html')
+        else:
+            return redirect(url_for('login'))
+
+    if request.method == 'POST':
+        username = session.get('user')
+        data = request.form.to_dict()
+        bucket_name = data.get('bucket_name')
+        description = data.get('description')
+        data = dict(bucket=True, user=username, bucket_name=bucket_name, description=description)
+        response = create_data(**data)
+
+        if response.message:
+            return jsonify({'success': response.message})
+
+        return make_response(jsonify({'error': response.error_message}), 500)
+
+
+@app.route('/view_buckets')
+def view_buckets():
+    if request.method == 'GET':
+        if 'user' in session.keys():
+            return render_template('view_bucket.html')
+
+
+@app.route('/delete_bucket')
+def delete_bucket():
+    return
+
+
+@app.route('/add_item')
+def add_item():
+    return
+
+
+@app.route('/view_items')
+def view_item():
+    return
+
+
+@app.route('/update_item')
+def update_item():
+    return
+
+
+@app.route('/delete_item')
+def delete_item():
+    return
+
+
+@app.route('/add_activities')
+def add_activities():
+    return render_template('add_activities.html')
+
+
+class AbstractFeatures(object):
+    def __init__(self, *args, **kwargs):
+        self.details = kwargs
+        self.args = args
+        self.message = None
+        self.error_message = None
+        self.username = None
+        self.bucket_name = None
+        self.description = None
+        self.bucket = None
+        self.activity = None
+
+    def initialize(self):
+        map(lambda x: self.details.update(dict(x, )), self.args)
+        for key, value in self.details.items():
+            setattr(self, key, value)
+
+    def create_data_(self):
+        def add_bucket():
+            values = [
+                dict(user=self.username, bucket_name=self.bucket_name, description=self.description,
+                     created=date.today())
+            ]
+
+            if 'buckets' in db.keys():
+                db['buckets'].append(values)
+            else:
+                db['buckets'] = values
+            self.message = 'Data created successfully'
+            return self
+
+        def add_activity():
+            return self
+
+        if self.bucket:
+            return add_bucket()
+
+        elif self.activity:
+            return add_activity()
+
+    def read_data_(self):
+        pass
+
+    def update_data_(self):
+        pass
+
+    def delete_data_(self):
+        pass
 
 
 if __name__ == '__main__':
--- static/js/script.js
+++ static/js/script.js
@@ -1,212 +1,77 @@
-/**
- * Created by ridge on 6/24/17.
- */
-$.ajaxLoad = true;
 
-$.defaultPage = 'templates/core/dashboard.html';
+function signUp() {
+    var username = $("#username").val();
+    var password = $("#password").val();
+    var confirm_password = $("#confirm_password").val();
 
-$.mainContent = $("#ui-view");
-
-
-
-$(function () {
-    $('#sign_up').validate({
-        rules: {
-            'terms': {
-                required: true
-            },
-            'confirm_password': {
-                equalTo: '[name="password"]'
-            }
-        },
-        highlight: function (input) {
-            console.log(input);
-            $(input).parents('.form-line').addClass('error');
-        },
-        unhighlight: function (input) {
-            $(input).parents('.form-line').removeClass('error');
-        },
-        errorPlacement: function (error, element) {
-            $(element).parents('.input-group').append(error);
-            $(element).parents('.form-group').append(error);
-        }
-    });
-});
-
-$(function () {
-    $('#sign_in').validate({
-        highlight: function (input) {
-            console.log(input);
-            $(input).parents('.form-line').addClass('error');
-        },
-        unhighlight: function (input) {
-            $(input).parents('.form-line').removeClass('error');
-        },
-        errorPlacement: function (error, element) {
-            $(element).parents('.input-group').append(error);
-        }
-    });
-});
-
-function login(){
-    var token = $('input[name="csrfmiddlewaretoken"]').val();
-    var username = $('input[name="username"]').val();
-    var password = $('input[name="password"]').val();
-    var redirect_url = $('input[name="redirect_url"]').val();
-
-    if (!password || !username){
+    if (!username){
+        alert("Please enter your username");
+        return 0;
+    }
+    if (!password){
+        alert("Please enter your password");
+        return 0;
+    }
+    if (!confirm_password){
+        alert("Please confirm your password");
+        return 0;
+    }
+    if (confirm_password !== password){
+        alert("Passwords don't match");
         return 0;
     }
 
     $.ajax({
-        url: '/merchant/login/',
-        data: {username:username, password:password, redirect_url:redirect_url},
-        dataType: 'text',
+        url:'/register',
+        data: {username: username, password: password, confirm_password: confirm_password},
         type: 'POST',
-        beforeSend: function (xmlHTTPRequest) {
-            xmlHTTPRequest.setRequestHeader('X-CSRFToken', token);
-        },
+        dataType: 'text',
         success: function(response){
             var json = JSON.parse(response);
-            window.location.href = json.url;
+            alert(json.success);
+            window.location.href = '/create_bucket';
+
+        },
+        error: function (xhr){
+            var json = JSON.parse(xhr.responseText);
+            alert(json.error)
+        }
+
+    })
+
+
+}
+
+function login(){
+    var username = $("#username").val();
+    var password = $("#password").val();
+
+    if(!username){
+        alert("Please enter your username");
+        return 0;
+    }
+    if(!password){
+        alert("Please enter your password");
+        return 0;
+    }
+
+    var data = {
+        password:password,
+        username:username
+    };
+
+    $.ajax({
+        url: '/login',
+        data: data,
+        type: 'POST',
+        success: function(){
+            window.location.href = '/create_bucket';
         },
         error: function(xhr){
             var json = JSON.parse(xhr.responseText);
-            if (json.error === 'username'){
-                swal('Error!', 'Username does not exist', 'error');
-                return 0;
-            }
-            else if (json.error === 'password'){
-                swal('Error!', 'Incorrect Password', 'error');
-                return 0;
-            }
-
-
-        }
-    })
-}
-
-function signUp(){
-    var token = $('input[name="csrfmiddlewaretoken"]').val();
-    var username = $('input[name="username"]').val();
-    var full_name = $('input[name="full_name"]').val();
-    var phone = $('input[name="phone"]').val();
-    var email = $('input[name="email"]').val();
-    var password = $('input[name="password"]').val();
-    var confirm_password = $('input[name="confirm_password"]').val();
-
-    if (!(confirm_password === password)){
-        return 0;
-    }
-    if (!username ||!full_name || !phone ||!password ||!confirm_password){
-        return 0;
-    }
-    var post_data = {
-        username:username,
-        full_name:full_name,
-        phone:phone,
-        email:email,
-        password:password,
-        confirm_password:confirm_password
-    };
-
-    $.ajax({
-        url: '/merchant/register/',
-        data: post_data,
-        dataType: 'text',
-        type: 'POST',
-        beforeSend: function (xmlHTTPRequest) {
-            xmlHTTPRequest.setRequestHeader('X-CSRFToken', token);
-        },
-        success: function (response) {
-            var json = JSON.parse(response);
-            window.location.href = json.url;
-
-        },
-        error: function (xhr) {
-            var json = JSON.parse(xhr.responseText);
-            swal('Error!', json.error, 'error');
-        }
-    })
-}
-
-function saveShopWizardForm() {
-    var shop_name = $("#shop_name").val();
-    var shop_phone = $("#shop_phone").val();
-    var shop_email = $("#shop_email").val();
-    var location = $("#location").val();
-    var company_name = $("#company_name").val();
-    var reg_no = $("#registration_number").val();
-    var business_website = $("#business_website").val();
-    var main_contact = $("#address").val();
-    var cash = $("#cash").checked;
-    var mpesa = $("#m-pesa").checked;
-    var airtel = $("#airtel-money").checked;
-    var equitel = $("#equitel").checked;
-    var visa = $("#visa-card").checked;
-    var master = $("#master-card").checked;
-    var csrf_token = $("input[name='csrfmiddlewaretoken']").val();
-    var alt_phone = $("#shop_alt_phone").val();
-
-    if (!shop_name){
-        swal('Error!', 'Please enter the shop name', 'error');
-        return 0;
-    }
-
-    if (!shop_phone){
-        swal('Error!', 'Please enter the shop phone', 'error');
-        return 0;
-    }
-
-    if (!location){
-        swal('Error!', 'Please enter the location', 'error');
-        return 0;
-    }
-
-    if (!csrf_token){
-        swal('Warning!', 'Please refresh the page and re-enter details', 'warning');
-        return 0;
-    }
-    var data = {
-        shop_name:shop_name,
-        shop_email:shop_email,
-        shop_phone:shop_phone,
-        location:location,
-        company_name:company_name,
-        reg_no:reg_no,
-        business_website:business_website,
-        main_contact:main_contact,
-        cash:cash,
-        mpesa:mpesa,
-        airtel:airtel,
-        visa:visa,
-        master:master,
-        alt_phone:alt_phone
-    };
-
-    $.ajax({
-        url: '/merchant/shop-setup/',
-        data: data,
-        dataType: 'text',
-        type: 'POST',
-        beforeSend: function (xmlHTTPRequest) {
-            xmlHTTPRequest.setRequestHeader('X-CSRFToken', csrf_token);
-        },
-        success: function (response) {
-            var json = JSON.parse(response);
-            swal({
-                title: 'Success!',
-                text: 'You will be contacted by Price Poa Agents to confirm. Meanwhile you can add products',
-                type: 'success',
-                timer: 2000
-            });
-            window.location.href = json.url;
-        },
-        error: function (xhr) {
-            var json = JSON.parse(xhr.responseText);
-            swal('Error!', json.error, 'error');
+            alert(json.error);
             return 0;
         }
-    });
-
+    })
 }
+
